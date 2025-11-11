1843c08884e87298833a9ddbce634036428759d2
christopherchiang <chiangkokfuan@gmail.com>
2025-11-11 20:16:19 +0800

    updated version 111125

--- lang/en/spe.php
+++ lang/en/spe.php
@@ -2,9 +2,9 @@
 // mod/spe/lang/en/spe.php
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Self & Peer Evaluation';
-$string['modulename'] = 'Self & Peer Evaluation';
-$string['modulenameplural'] = 'Self & Peer Evaluations';
+$string['pluginname'] = 'Self/Peer Evaluation';
+$string['modulename'] = 'Self/Peer Evaluation';
+$string['modulenameplural'] = 'Self/Peer Evaluations';
 $string['pluginadministration']  = 'SPE administration';
 $string['pluginname_help'] = 'Collect self & peer ratings and comments for group projects. Includes sentiment analysis.';
 $string['spe:addinstance'] = 'Add a new Self & Peer Evaluation activity';
@@ -12,10 +12,6 @@ $string['spe:view'] = 'View SPE activity';
 $string['spe:submit'] = 'Submit self & peer evaluations';
 $string['spe:viewreports'] = 'View SPE reports';
 $string['spe:manage']      = 'Manage SPE settings';
-$string['pluginname'] = 'Self & Peer Evaluation (SPE)';
-$string['modulename'] = 'Self & Peer Evaluation';
-$string['modulenameplural'] = 'Self & Peer Evaluations';
-$string['pluginadministration'] = 'SPE administration';
 $string['sentiment_url'] = 'Sentiment API URL';
 $string['sentiment_token'] = 'Sentiment API token';
 $string['analysisreport'] = 'Sentiment Analysis Report';
